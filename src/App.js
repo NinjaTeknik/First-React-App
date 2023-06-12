3d85220a4a8b0bd7fc001441131264113c55372b
Rasmus <rasm50m1@edu.campusvejle.dk>
2023-06-12 11:58:01 +0200

    Immutability of Props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
 //import Hello from './components/Hello';
-import Message from './components/Message';
-//import Profile from './components/Profile';
+//import Message from './components/Message';
+import Profile from './components/Profile';
 
 function App() {
   return ( 
   <div className="App">
-    <Message messagecode="10" messagecontent="This is a message from props"/>
+    <Profile name="NN" lastnaame="N.N."/>
   </div>
   );
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,7 @@
 function Profile(props) {
   console.log(props);
+  //Can't change props value when it's set. 
+  //props.name = "John";
   return (
     <h1>
       Name: {props.name} {props.lastname}
