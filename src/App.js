0dd09ff36eccdc4b40fd65d640443635e1204c0c
Rasmus <rasm50m1@edu.campusvejle.dk>
2023-06-12 13:30:00 +0200

    Added state and increment to Counter.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 //import Hello from './components/Hello';
 //import Message from './components/Message';
-import Profile from './components/Profile';
+//import Profile from './components/Profile';
+import Counter from './components/Counter';
 
 function App() {
   return ( 
   <div className="App">
-    <Profile name="NN" lastname="N.N."/>
+    <Counter></Counter>
   </div>
   );
 }
