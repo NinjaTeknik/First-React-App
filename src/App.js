3f8ea5e5290151ecfdb982f973130dd06e2037fe
EOGKevin <104348441+EOGKevin@users.noreply.github.com>
2023-06-13 11:20:14 +0200

    Added: Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 //import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 //import Hello from './components/Hello';
 //import Message from './components/Message';
 //import Profile from './components/Profile';
-import Counter from './components/Counter';
-import FunctionalCounter from './components/FunctionalCounter';
+//import Counter from './components/Counter';
+//import FunctionalCounter from './components/FunctionalCounter';
 //import Resume from './components/Resume';
 //import FunctionEvent from './components/FunctionEvent';
 //import ClassEvent from './components/ClassEvent';
+import ConditionalComponent from "./components/ConditionalComponent";
 
 function App() {
-  return ( 
-  <div className="App">
-    <FunctionalCounter></FunctionalCounter>
-  </div>
+  return (
+    <div className="App">
+      <ConditionalComponent></ConditionalComponent>
+    </div>
   );
 }
 
---
+++ src/components/ConditionalComponent.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+//import Counter from "./Counter";
+//import FunctionalCounter from "./FunctionalCounter";
+export default function ConditionalComponent() {
+  const [display, setDisplay] = useState(true);
+
+  let output;
+  if (display) {
+    output = <h3>This is a conditional component</h3>;
+  } else {
+    output = <h3>Nothing to see here</h3>;
+  }
+  return <div>{output}</div>;
+}
